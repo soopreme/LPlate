7c49c16da121911257f7e338cae23d2b10bafb75
Ethan O'Brien <eobsite1@gmail.com>
2020-04-06 23:48:14 +0100

    added token generation and return to register route added login route

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mysql = require('mysql');
 const sanitize = require('sanitize')();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
 var app = express();
 var connection = mysql.createConnection({
@@ -12,6 +14,8 @@ var connection = mysql.createConnection({
 });
 
 const saltRounds = 10;
+const privateKey = fs.readFileSync(process.env.PRIVKEYPATH);
+const publicKey = fs.readFileSync(process.env.PUBKEYPATH);
 
 connection.connect();
 
@@ -31,17 +35,40 @@ var query = query => new Promise((resolve, reject) => {
     });
 });
 
-var hash = string => new Promise((resolve, reject) => {
+var genhash = string => new Promise((resolve, reject) => {
     bcrypt.genSalt(saltRounds, (err, salt) => {
         if(err) {
             reject({code: 500, err});
         }
-        bcrypt.hash(string, salt, (err, hsh) => {
+        bcrypt.hash(string, salt, (err, hash) => {
             if(err) {
                 reject({code: 500, err});
             }
-            resolve(hsh);
+            resolve(hash);
         })
+    })
+});
+
+var checkhash = (string, hash) => new Promise((resolve, reject) => {
+    bcrypt.compare(string, hash, (err, result) => {
+        if(err) {
+            reject({code: 500, err});
+        }
+        if(result) {
+            resolve(result);
+        } else {
+            reject({code: 400});
+        }
+    })
+});
+
+var gentoken = userRow => new Promise((resolve, reject) => {
+    var { username, password, ID } = userRow;
+    jwt.sign({ username, password, ID }, privateKey, { algorithm: 'RS256' }, (err, token) => {
+        if(err) {
+            reject({code: 500, err});
+        }
+        resolve(token);
     })
 });
 
@@ -62,16 +89,43 @@ app.post('/register', (req, res) => {
         if(result) {
             reject({code: 409, err:"Email is already in use"});
         }
-        return hash(password);
+        return genhash(password);
     })
     .then(passwordHashed => {
         return query(`INSERT INTO 'users' (email, username, password) VALUES ('${emailSanitized}', '${usernameSanitized}', '${passwordHashed}')`);
     })
-    .then(() => {
-        return res.status(200).json({msg: "Success"});
+    .then(result => {
+        return gentoken(result);
+    })
+    .then(token => {
+        return res.status(200).json({token});
     })
     .catch(err => {
-        res.status(err.code).json({err: err.err});
+        return res.status(err.code).json({err: err.err});
+    });
+});
+
+app.post('/login', (req, res) => {
+    var {username, password} = req.body;
+    var usernameSanitized = sanitize.value(username, 'string');
+    var userObject;
+
+    query(`SELECT * FROM 'users' WHERE username='${usernameSanitized}'`)
+    .then(result => {
+        if(!result) {
+            reject({code: 401, err: "User not found"});
+        }
+        userObject = result;
+        return checkhash(password, result.password);
+    })
+    .then(result => {
+        return gentoken(result);
+    })
+    .then(token => {
+        return res.status(200).json({token});
+    })
+    .catch(err => {
+        return res.status(err.code).json({err: err.err});
     });
 });
 
